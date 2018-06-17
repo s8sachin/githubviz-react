87dcefee24046239f42dbc7af4d8d2c57077e712
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-06-17 15:43:12 +0530

    header component

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
 import { Grid, Jumbotron, Button } from 'react-bootstrap';
+import Header from './commons/Header';
 
 class Root extends Component {
 
   render() {
     return (
-      <Grid bsClass='container'>
-        <Jumbotron>
-          <h1>Hello, world!</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
-          <p>
-            <Button bsStyle="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
-      </Grid>
+      <div>
+        <Header/>
+        <Grid bsClass='container'>
+          <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+            <p>
+              <Button bsStyle="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+        </Grid>
+      </div>
     );
   }
 }
