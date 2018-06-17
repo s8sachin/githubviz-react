a166d204b8f6402d40ab3d369fdfb752fe0f848b
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-06-17 13:34:45 +0530

    redux and path setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { Route, Router, Switch, Redirect } from 'react-router-dom';
+import browserHistory from './history';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Switch>
+        <Route exact path="/" component={App}/>
+      </Switch>
+    </Router>
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
