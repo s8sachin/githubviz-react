295b2ee2dd8e9f3013b011d60380448bb13dadb2
deepikacs <dshivalingegowda@qwinix.io>
2018-06-21 09:34:09 +0530

    piechart7

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React, { Component } from 'react';
 import Root from './components/Root';
 import './App.css';
 
-
 class App extends Component {
   render() {
-   
     return (
       <Root/>
-      
     );
   }
 }
