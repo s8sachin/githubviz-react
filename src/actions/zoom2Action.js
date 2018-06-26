5822edd683384222b0551df16a066279806f54f7
chytramn59 <cnagaraju@qwinix.io>
2018-06-26 15:11:18 +0530

    zoom2 graph2 redux

--- src/actions/zoom2Action.js
+++ src/actions/zoom2Action.js
@@ -1,5 +1,5 @@
-import { singleRepoNCommits } from '../api/zoom2Api';
-import { SINGLE_REPO_COMMITS } from './type';
+import { singleRepoNCommits, singleUserNCommits} from '../api/zoom2Api';
+import { SINGLE_REPO_COMMITS, SINGLE_USERN_COMMITS} from './type';
 
 export const singleRepoNCommitsAction = (status) => {
   return (dispatch) => {
@@ -8,6 +8,18 @@ export const singleRepoNCommitsAction = (status) => {
       const single_repo_commits = 'single_repo_commits'
       const data = response.data.singleRepoNCommits;
       dispatch({type: SINGLE_REPO_COMMITS, payload: {single_repo_commits, data}})
+    })
+    .catch(er => console.log(er))
+  }
+}
+export const  singleUserNCommitsAction = (status) => {
+  return (dispatch) => {
+    console.log(status)
+    singleUserNCommits(status)
+    .then(response => {
+      const single_usern_commits = 'single_usern_commits'
+      const data = response.data.singleUserNCommits;
+      dispatch({type: SINGLE_USERN_COMMITS, payload: {single_usern_commits, data}})
     })
     .catch(er => console.log(er))
   }
