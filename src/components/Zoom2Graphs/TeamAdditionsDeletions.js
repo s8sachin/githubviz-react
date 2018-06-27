784d074fa8dd096993c7a204ee7077427d798051
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-06-27 16:59:10 +0530

    indentation fixes

--- src/components/Zoom2Graphs/TeamAdditionsDeletions.js
+++ src/components/Zoom2Graphs/TeamAdditionsDeletions.js
@@ -19,48 +19,48 @@ class TeamAdditionsDeletions extends Component {
     return (
       <div>
         <Header/>
-      <div style={{'paddingLeft': '50px'}}>
-      <Grid>
-       <Row className="show-grid">
-         <Col xs={12} md={2}>
-          <Button className="butn-top" bsStyle="primary" onClick={() => browserHistory.push('/AllGraphs')}>Back</Button>
-         </Col>
-         <Col xs={12} md={6}>
-          <h4 className="header-color1">Team Members Additions and deletions from recent 10 PRs</h4>
-         </Col>
-         <Col xs={12} md={4}></Col>
-         </Row>
-         <Row className="show-grid">
-            <Col className="margin-row" xs={12} md={12}>
-              <XYPlot
-          xType="ordinal"
-          width={1000}
-          height={500}
-          xDistance={100}>
-          <VerticalGridLines />
-          <HorizontalGridLines />
-          <XAxis />
-          <YAxis />
-          <VerticalBarSeries 
-            data={this.props.team_additons_deletions.additions}
-            onValueMouseOver={v => this.setState({ mouseOverValue: v })}
-            onValueMouseOut={v => this.setState({ mouseOverValue: false })}/>
-          <VerticalBarSeries 
-            data={this.props.team_additons_deletions.deletions}
-            onValueMouseOver={v => this.setState({ mouseOverValue: v })}
-            onValueMouseOut={v => this.setState({ mouseOverValue: false })}/>
-          { this.state.mouseOverValue && 
-            <Hint value={this.state.mouseOverValue}>
-              <div className='hintStyle'>
-                <p>{this.state.mouseOverValue.x} <br/> {this.state.mouseOverValue.type}: <br/>{this.state.mouseOverValue.y}</p>
-              </div>
-            </Hint>
-          }
-          </XYPlot>
-         </Col>
-        </Row>
-        </Grid>
-      </div>
+        <div style={{'paddingLeft': '50px'}}>
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={12} md={2}>
+                <Button className="butn-top" bsStyle="primary" onClick={() => browserHistory.push('/AllGraphs')}>Back</Button>
+              </Col>
+              <Col xs={12} md={6}>
+                <h4 className="header-color1">Team Members Additions and deletions from recent 10 PRs</h4>
+              </Col>
+              <Col xs={12} md={4}></Col>
+            </Row>
+            <Row className="show-grid">
+              <Col className="margin-row" xs={12} md={12}>
+                <XYPlot
+                  xType="ordinal"
+                  width={1000}
+                  height={500}
+                  xDistance={100}>
+                  <VerticalGridLines />
+                  <HorizontalGridLines />
+                  <XAxis />
+                  <YAxis />
+                  <VerticalBarSeries 
+                    data={this.props.team_additons_deletions.additions}
+                    onValueMouseOver={v => this.setState({ mouseOverValue: v })}
+                    onValueMouseOut={v => this.setState({ mouseOverValue: false })}/>
+                  <VerticalBarSeries 
+                    data={this.props.team_additons_deletions.deletions}
+                    onValueMouseOver={v => this.setState({ mouseOverValue: v })}
+                    onValueMouseOut={v => this.setState({ mouseOverValue: false })}/>
+                  { this.state.mouseOverValue && 
+                    <Hint value={this.state.mouseOverValue}>
+                      <div className='hintStyle'>
+                        <p>{this.state.mouseOverValue.x} <br/> {this.state.mouseOverValue.type}: <br/>{this.state.mouseOverValue.y}</p>
+                      </div>
+                    </Hint>
+                  }
+                </XYPlot>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
       </div>
     )
   }
