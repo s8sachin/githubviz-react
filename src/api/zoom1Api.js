9fd9fb72485cf378af0ed50fd48b3f8bb715f1c0
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-06-27 16:19:46 +0530

    z1 graph 1 n 2 dropdown ajax done

--- src/api/zoom1Api.js
+++ src/api/zoom1Api.js
@@ -2,17 +2,19 @@ import axios from 'axios';
 import { ROOT_URL, headers } from './api_config';
 
 export const getReposAndCommits = (params) => {
+  const queryParams = params || {};
   return axios({
     method:'get',
-    url:`${ROOT_URL}/reposAndCommits`,
+    url:`${ROOT_URL}/reposAndCommits?repoCount=${queryParams.repoCount}`,
     headers: headers()
   })
 }
 
 export const getUsersAndPullreq = (params) => {
+  const queryParams = params || {};
   return axios({
     method:'get',
-    url:`${ROOT_URL}/usersAndPullreq`,
+    url:`${ROOT_URL}/usersAndPullreq?usersCount=${queryParams.usersCount}`,
     headers: headers()
   })
 }
--- src/components/Zoom1Graphs/RepoNCommits.js
+++ src/components/Zoom1Graphs/RepoNCommits.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { XYPlot, XAxis, YAxis, VerticalBarSeries, VerticalGridLines, HorizontalGridLines, Hint } from 'react-vis';
 import { connect } from 'react-redux';
+import { DropdownButton, MenuItem, Tooltip } from 'react-bootstrap';
 import browserHistory from '../../history';
+import { repoAndCommitsAction } from '../../actions/AllGraphAction';
 
 const lodingGraphData = [
   {x: 0, y: 8},
@@ -18,13 +20,15 @@ const lodingGraphData = [
   {x: 8, y: 2},
   {x: 9, y: 0}
 ];
+
+const repoCounts = [20, 40, 60, 80, 100]
 class RepoNCommits extends Component {
   constructor() {
     super();
   }
 
   componentWillMount() {
-    this.setState({ mouseOverValue: false })
+    this.setState({ mouseOverValue: false, repoCount: this.props.repoCount })
   }
 
   singleRepoNCommits = (v) => {
@@ -32,13 +36,26 @@ class RepoNCommits extends Component {
     browserHistory.push(`/singleRepoNCommits/${repo}`);
   }
 
+  changeNumberOfRepos (count) {
+    this.props.repoAndCommitsAction({repoCount: count})
+    this.setState({repoCount: count})
+  }
+
   render() {
     var graphData = this.props.repos_and_commits[0] ? this.props.repos_and_commits : lodingGraphData;
     return (
       <div>
-        <XYPlot height={300} width={1000} color="orange" stroke="black" xType="ordinal" className={this.props.repos_and_commits[0] ? '' : 'loadingGraphOpacity'}>
+        <div className='pull-right zIndex100'>
+          <span className='marginRight30'><b>Repositories Selected: {this.state.repoCount}</b></span>
+          <DropdownButton title='Repositories Count' id='g1Dropdown' onSelect={v => this.changeNumberOfRepos(v)}>
+            {repoCounts.map((count, index) => {
+              return <MenuItem key={index} active={this.state.repoCount === count} eventKey={count}>{count}</MenuItem>
+            })}
+          </DropdownButton>
+        </div><br/>
+        <XYPlot height={300} width={1000} color="orange" stroke="black" xType="ordinal" className={this.props.repos_and_commits[0] ? 'rv-xy-plot__inner_380' : 'loadingGraphOpacity rv-xy-plot__inner_380'}>
           <HorizontalGridLines />
-          <XAxis tickLabelAngle={-45} />
+          <XAxis tickLabelAngle={-45}/>
           <YAxis />
           <VerticalBarSeries data={graphData}
             onValueClick={v => this.singleRepoNCommits(v)}
@@ -62,4 +79,4 @@ const mapStateToProps = (state) => {
   return { repos_and_commits };
 }
 
-export default connect(mapStateToProps)(RepoNCommits);
+export default connect(mapStateToProps, { repoAndCommitsAction })(RepoNCommits);
