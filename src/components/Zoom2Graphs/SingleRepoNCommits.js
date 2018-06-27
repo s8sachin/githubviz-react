af42296eba1752307cee4cc89982407593538538
deepikacs <dshivalingegowda@qwinix.io>
2018-06-27 17:14:10 +0530

    z2g1 ui fix

--- src/components/Zoom2Graphs/SingleRepoNCommits.js
+++ src/components/Zoom2Graphs/SingleRepoNCommits.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { XYPlot, XAxis, YAxis, VerticalBarSeries, VerticalGridLines, HorizontalGridLines, Hint, LineMarkSeries } from 'react-vis';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, Hint, LineMarkSeries } from 'react-vis';
 import { singleRepoNCommitsAction } from '../../actions/zoom2Action';
 import browserHistory from '../../history';
 import {Row,Col,Grid,Button} from "react-bootstrap"
@@ -26,52 +26,51 @@ class SingleRepoNCommits extends Component {
  
   componentWillMount() {
     const repo = this.props.match.params.repo;
-    this.setState({ repoName: repo});
+    this.setState({ repoName: repo, mouseOverValue: false });
     this.props.singleRepoNCommitsAction(repo);
-    this.setState({ mouseOverValue: false });
   }
-  back = () => {
+  back () {
     browserHistory.push(`/allGraphs`);
   }
 
   render () {
     var graphData = this.props.single_repo_commits[0] ? this.props.single_repo_commits : lodingGraphData;
     return (
-        <div className="overflow">
-          <Header />
-           <center> 
-            <Grid>
-              <Row className="show-grid">
-                <Col xs={12} md={2}>
-                  <Button bsStyle="primary" className="backbtn" onClick={this.back}>Back</Button>
-                </Col>
-                <Col xs={12} md={10}>
-                <h2>{this.state.repoName} branches and commits</h2>
-                </Col>
-              </Row>
-              <Row className="show-grid">
-                <Col md={12}>
-                  <XYPlot height={500} width={1200} color="#25939a" stroke="#25939a" xType="ordinal" className={this.props.single_repo_commits[0] ? '' : 'loadingGraphOpacity'}>
-                    <HorizontalGridLines />
-                    <XAxis tickLabelAngle={-45} title="Branches"/>
-                    <YAxis title="Commits" />
-                    <LineMarkSeries data={graphData}
+      <div className="overflow1">
+        <Header />
+        <center>
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={12} md={4}>
+                <Button bsStyle="primary" className="backbtn" onClick={() => this.back()}>Back</Button>
+              </Col>
+              <Col xs={12} md={8}>
+                <h3 className='graph-heading1'>{this.state.repoName} branches and commits</h3>
+              </Col>
+            </Row>
+            <Row className="show-grid">
+              <Col md={12}>
+                <XYPlot height={500} width={1200} color="#25939a" stroke="#25939a" xType="ordinal" className={this.props.single_repo_commits[0] ? '' : 'loadingGraphOpacity'}>
+                  <HorizontalGridLines />
+                  <XAxis tickLabelAngle={-45} title="Branches" />
+                  <YAxis title="Commits" />
+                  <LineMarkSeries data={graphData}
                     onValueMouseOver={v => this.setState({ mouseOverValue: v })}
-                      onSeriesMouseOut={v => this.setState({ mouseOverValue: false })} />
-                      {this.state.mouseOverValue &&
-                        <Hint value={this.state.mouseOverValue}>
-                          <div className='hintStyle'>
-                            <p>{this.state.mouseOverValue.x} :<br />{this.state.mouseOverValue.y}</p>
-                          </div>
-                        </Hint>
-                      } 
-                  </XYPlot>
-                </Col>
-              </Row>
-            </Grid>
-          </center>
+                    onSeriesMouseOut={v => this.setState({ mouseOverValue: false })} />
+                  {this.state.mouseOverValue &&
+                    <Hint value={this.state.mouseOverValue}>
+                      <div className='hintStyle'>
+                        <p>{this.state.mouseOverValue.x} :<br />{this.state.mouseOverValue.y}</p>
+                      </div>
+                    </Hint>
+                  }
+                </XYPlot>
+              </Col>
+            </Row>
+          </Grid>
+        </center>
       </div>
-   )
+    )
   }
 }
 
