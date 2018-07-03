46b6072827cdbdb0a2a6806b5a9e1f566d7b0a1e
deepikacs <dshivalingegowda@qwinix.io>
2018-07-03 09:48:37 +0530

    tabel2data ui

--- src/components/Zoom3Graphs/table2Data.js
+++ src/components/Zoom3Graphs/table2Data.js
@@ -3,6 +3,8 @@ import {Table } from 'react-bootstrap';
 import { singlePullreqNcommitsAction } from '../../actions/zoom3Action';
 import { connect } from 'react-redux';
 import Header from '../commons/Header';
+import { Col, Grid, Row, Button, Glyphicon }   from 'react-bootstrap';
+import browserHistory from '../../history';
 
 class Table2Data extends Component {
     componentWillMount() {
@@ -13,32 +15,47 @@ class Table2Data extends Component {
   render() {
     return (
         <div>
-        <Header/>
-        <Table striped bordered condensed hover>
-        <thead>
-          <tr>
-            <th>Oid</th>
-            <th>Message</th>
-            <th>Author</th>
-          </tr>
-        </thead>
-        <tbody>
-        {this.props.table2_data && this.props.table2_data.map((obj, index) => {
-            return(<tr key={index}>
-            <td>{obj.oid}</td>
-            <td>{obj.message}</td>
-            <td>{obj.author}</td>
-          </tr>)
-        })}
-        </tbody>
-      </Table>
-      </div>
+          <Header/>
+          <Grid>
+            <Row className="show-grid">
+              <center>
+                <Col xs={12} md={2}>
+                <Button className="butn-top" bsStyle="primary" onClick={() => browserHistory.push('/singleUserNCommits/:label')}><Glyphicon glyph="chevron-left" /> Back</Button>
+                </Col>
+               <Col xs={12} md={10}>
+                <h4 className="header-color1">single user pullrequest and commits </h4>
+               </Col>
+              </center>
+            </Row>
+            <Row className="show-grid">
+              <Col className="margin-row" xs={12} md={12}>
+                <Table striped bordered condensed hover>
+                  <thead>
+                  <tr>
+                    <th>Oid</th>
+                    <th>Message</th>
+                    <th>Author</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                   {this.props.table2_data && this.props.table2_data.map((obj, index) => {
+                      return(<tr key={index}>
+                      <td>{obj.oid}</td>
+                      <td>{obj.message}</td>
+                      <td>{obj.author}</td>
+                     </tr>)
+                    })}
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
     );
   }
 }
 const mapStateToProps = (state) => {
-    const { table2_data } = state.zoom3;
-    return { table2_data };
+  const { table2_data } = state.zoom3;
+  return { table2_data };
 }
-  
-  export default connect(mapStateToProps, { singlePullreqNcommitsAction })(Table2Data);
+export default connect(mapStateToProps, { singlePullreqNcommitsAction })(Table2Data);
