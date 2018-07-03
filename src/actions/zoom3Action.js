3d74a8d0497fef235ddc8bdfbb41e35e2eb8bf57
chytramn59 <cnagaraju@qwinix.io>
2018-07-03 12:56:08 +0530

    indent

--- src/actions/zoom3Action.js
+++ src/actions/zoom3Action.js
@@ -3,13 +3,11 @@ import { TABLE_DATA} from './type';
 
 export const committedDateNMessageAction = (status) => {
     return (dispatch) => {
-        console.log(status)
       const table_data = 'table_data';
       var data = [];
       dispatch({type:  TABLE_DATA, payload: {table_data, data}});
       committedDateNMessage (status)
       .then(response => {
-          console.log(response)
         data = response.data.committedDateNMessage ;
         dispatch({type: TABLE_DATA, payload: {table_data, data}})
       })
