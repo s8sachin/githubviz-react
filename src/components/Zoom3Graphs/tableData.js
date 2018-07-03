c94a7d9eaf7b0714bda186bd4017e5b078c11b8b
chytramn59 <cnagaraju@qwinix.io>
2018-07-03 14:53:36 +0530

    ui

--- src/components/Zoom3Graphs/tableData.js
+++ src/components/Zoom3Graphs/tableData.js
@@ -14,32 +14,32 @@ class TableData extends Component {
   render() {
     return (
         <div className="table-custom">
-        <Header/>
-        <Grid>
-          <Row>
-          <Col xs={12} md={4} className="margin-top">
-          <Button className="butn-top" bsStyle="primary" onClick={() => browserHistory.push('/singleRepoNCommits/:repo')}><Glyphicon glyph="chevron-left" /> Back</Button>
-          </Col>
-          <Col xs={12} md={8}>
-          <h4 className="header-color1">single user committedDate and Id </h4>
-          </Col>
-          </Row>
+          <Header/>
+            <Grid>
+             <Row>
+               <Col xs={12} md={4} className="margin-top">
+                 <Button className="butn-top" bsStyle="primary" onClick={() => browserHistory.push('/singleRepoNCommits/:repo')}><Glyphicon glyph="chevron-left" /> Back</Button>
+              </Col>
+              <Col xs={12} md={8}>
+               <h4 className="header-color1">single user committedDate and Id </h4>
+              </Col>
+             </Row>
           <Row>
           <Col xs={12} md={12}>
-        <Table striped bordered condensed hover>
-        <thead>
-          <tr>
-            <th>commited date</th>
-            <th>Message</th>
-            <th>oid</th>
-          </tr>
-        </thead>
-        <tbody>
+           <Table striped bordered condensed hover>
+             <thead>
+               <tr>
+                <th>commited date</th>
+                <th>Message</th>
+                <th>oid</th>
+              </tr>
+            </thead>
+         <tbody>
         {this.props.table_data && this.props.table_data.map((obj, index) => {
             return(<tr key={index}>
-            <td>{obj.committedDate}</td>
-            <td>{obj.message}</td>
-            <td>{obj.id}</td>
+              <td>{obj.committedDate}</td>
+              <td>{obj.message}</td>
+              <td>{obj.id}</td>
           </tr>)
         })}
         </tbody>
